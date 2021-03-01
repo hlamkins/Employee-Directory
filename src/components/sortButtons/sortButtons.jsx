6acbd18acef451819d2f60eac3d6e8f0ba6f8933
Heather Lamkins <lamkinsheather@gmail.com>
2021-02-28 21:02:45 -0600

    sort button jsx complete

--- src/components/sortButtons/sortButtons.jsx
+++ src/components/sortButtons/sortButtons.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 const SortButtons = (props) => {
     return (
-        
-    )
-}
+        <div className="btn-group" role="group">
+            <button onClick={() => props.sortByName("first")} type="button" className="btn btn-outline-dark">Sort by First Name</button>
+            <button onClick={() => props.sortByName("last")} type="button" className="btn btn-outline-dark">Sort by Last Name</button>
+        </div>
+    );
+};
+
+export default SortButtons;
